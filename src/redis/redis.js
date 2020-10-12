53a963257a2037dab05a246f9956e360125fbc44
alexguo8 <37782734+alexguo8@users.noreply.github.com>
2020-10-11 20:38:10 -0400

    Redis leaderboard (#17)
    * Add redis to docker-compose
    
    * Add node-redis client
    
    * Add base leaderboard functions for redis
    
    * Persist storage for redis
    
    * Restructure using async/await
    
    * Restructure files
    
    * Add global leaderboard
    
    Co-authored-by: Tony Zhao <41414030+tawnyzhao@users.noreply.github.com>
    Co-authored-by: Alex Guo <alexguo@Alexs-MacBook-Pro.local>

---
+++ src/redis/redis.js
@@ -0,0 +1,13 @@
+const { promisify } = require("util");
+const redisModule = require('redis');
+const client = redisModule.createClient({ host: 'building-up-redis', port: 6379 });
+
+const redis = {
+    get: promisify(client.get).bind(client),
+    set: promisify(client.set).bind(client),
+    zadd: promisify(client.zadd).bind(client),
+    zincrby: promisify(client.zincrby).bind(client),
+    zrevrange: promisify(client.zrevrange).bind(client)
+}
+
+exports.redis = redis;
