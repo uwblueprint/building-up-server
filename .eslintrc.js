51b01e7365a888f2ba3cb1ec9e896f8db716737a
anandissac <anand.z.issac@gmail.com>
2020-10-15 20:32:20 -0400

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,4 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    root: true,
+    extends: '@react-native-community',
 };
