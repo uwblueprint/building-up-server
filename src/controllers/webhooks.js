9fe2ac5fb51b4a6f16d47fbdf948efc03a0d5557
Michael Meng <michaelmeng998@gmail.com>
2020-10-27 23:44:09 -0400

    Shopify-webhooks made some changes to webhook response logic

--- src/controllers/webhooks.js
+++ src/controllers/webhooks.js
@@ -2,11 +2,11 @@ const models = require("../models");
 
 const shopifyWebhook = (req, res) => {
   let event;
+
   try {
     event = JSON.parse(req.body);
   } catch (err) {
     console.error(`Webhook Error: ${err.message}`);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
   var userID, teamID;
@@ -15,20 +15,18 @@ const shopifyWebhook = (req, res) => {
   if (typeof event.note_attributes[0] !== "undefined") {
     userID = event.note_attributes[0].value;
   } else {
+    // we still want to track the order for completeness even if the userID was NULL
+    userID = null
     console.error("Error: undefined value in JSON payload");
-    return res
-      .status(400)
-      .send(`Webhook Error: undefined value in JSON payload`);
   }
 
   // teamID stored in second index
   if (typeof event.note_attributes[1] !== "undefined") {
     teamID = event.note_attributes[1].value;
   } else {
+    // we still want to track the order for completeness even if the teamID was NULL
+    teamID = null
     console.error("Error: undefined value in JSON payload");
-    return res
-      .status(400)
-      .send(`Webhook Error: undefined value in JSON payload`);
   }
   //includes any discounts, excludes tax
   const price = event.subtotal_price;
