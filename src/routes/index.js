d9c259a04dadf757df38a3ad7510316c1f036085
Michael Meng <michaelmeng998@gmail.com>
2020-11-15 18:39:48 -0500

    shopify-webhooks added shopify header validatio middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,25 +6,37 @@ const {
 } = require("../controllers");
 
 const bodyParser = require("body-parser");
+const { shopifyValidator } = require("../middleware/shopifyValidator");
 
 const router = express.Router();
 
-router.post(
-  "/captureOrderWebhook",
-  bodyParser.raw({ type: "application/json" }),
-  captureOrderWebhook
+/*
+bodyParser middleware for verifying the request type and setting the result of the
+shopifyValidator in the header.
+*/
+router.use(
+  bodyParser.raw({
+    type: "application/json",
+    verify: shopifyValidator
+  })
 );
 
-router.post(
-  "/cancelOrderWebhook",
-  bodyParser.raw({ type: "application/json" }),
-  cancelOrderWebhook
-);
+/*
+middleware for verifying the result of the shopifyValidator set in the
+request header.
+*/
+router.use((req, res, next) => {
+  if (req.is_shopify_header_verified == true) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
 
-router.post(
-  "/updateOrderWebhook",
-  bodyParser.raw({ type: "application/json" }),
-  updateOrderWebhook
-);
+router.post("/captureOrderWebhook", captureOrderWebhook);
+
+router.post("/cancelOrderWebhook", cancelOrderWebhook);
+
+router.post("/updateOrderWebhook", updateOrderWebhook);
 
 module.exports = router;
