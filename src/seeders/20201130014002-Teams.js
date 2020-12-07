3570f18f2082265dcc38398bcd03566a8cd11062
Faizaan Madhani <fzmadhani@gmail.com>
2020-12-06 21:19:27 -0500

    Teams seed data{

---
+++ src/seeders/20201130014002-Teams.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Teams', [
+    {
+      name: "Test User 1",
+      organization: "BP",
+      amountRaised: 150,
+      itemsSold: 200,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: "Test User 2",
+      organization: "BP2",
+      amountRaised: 42809,
+      itemsSold: 424,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: "Test User 3",
+      organization: "BP3",
+      amountRaised: 42,
+      itemsSold: 96,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: "Test User 4",
+      organization: "BP4",
+      amountRaised: 1020,
+      itemsSold: 200,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: "Test User 5",
+      organization: "BP5",
+      amountRaised: 10090,
+      itemsSold: 200,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: "Test User 6",
+      organization: "BP6",
+      amountRaised: 1640,
+      itemsSold: 34500,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
