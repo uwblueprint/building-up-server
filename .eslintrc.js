048027d2de198e621a62baa46f8ac5967adfe4d2
Jeffrey Huang <jeff.huang62@gmail.com>
2021-01-28 12:46:43 -0500

    ESlint, Prettier, and VS Code config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-    root: true,
-    extends: '@react-native-community',
+  root: true,
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
