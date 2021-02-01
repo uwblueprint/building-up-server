08d7854ffced10d2e464c74f3dea0b15217bdf7d
Hong Yi Chen <hongyigma@gmail.com>
2021-01-31 21:07:15 -0500

    Updated users.js schema lint

--- src/schemas/users.js
+++ src/schemas/users.js
@@ -3,24 +3,22 @@ const { gql } = require('apollo-server-express');
 // type User has already been defined in auth.js schemas
 
 const usersTypeDefs = gql`
-   type User {
-    Int!
-    tName: String!
-    Name: String!
-    ail: String!
-    ssword: String!
-    : String!
-teamId: Int!
-}
-
-extend type Query {
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    role: String!
+    teamId: Int!
+  }
+  extend type Query {
     Users(teamId: Int!): [User]
-    getUser(id: Int!)User
-    llUsers: [User!]!
-  
-
-  tend type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!, role: String!, teamId: Int!) : User
+    getUser(id: Int!): User
+    getAllUsers: [User!]!
+  }
+  extend type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!, role: String!, teamId: Int!): User
     updateUser(
       id: Int!
       firstName: String
@@ -30,5 +28,8 @@ extend type Query {
       role: String
       teamId: Int
     ): User
-  };`;
+  }
+`;
+
+exports.usersTypeDefs = usersTypeDefs;
 exports.usersTypeDefs = usersTypeDefs;
