16cf58d2e43ed5b614a47790135e0b926122d9aa
kevinz999@gmail.com <kevinz999@gmail.com>
2021-02-14 16:44:36 -0500

    work in progress

--- src/models/team.js
+++ src/models/team.js
@@ -1,4 +1,28 @@
 const { Model } = require('sequelize');
+const models = require('../models');
+
+function generateID(){
+  let existing = await models.Team.findAll({
+    attributes: ["id"]
+  })
+  let existing_ids = Set();
+  for(let i = 0; i < existing.length; i++){
+    existing_ids.add(existing[i]["ids"]);
+  }
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var hash = "";
+  let hashNotUnique = true;
+  while(hashNotUnique){
+    for(let i = 0; i < 4; i++){
+      hash += letters.charAt(Math.floor(Math.random() * 26));
+    }
+    hashNotUnique = false
+    if(existing_ids.has(hash)){
+      hashNotUnique = true;
+    }
+  }
+  return hash;
+}
 
 module.exports = (sequelize, DataTypes) => {
   class Team extends Model {
@@ -23,5 +47,7 @@ module.exports = (sequelize, DataTypes) => {
       modelName: 'Team',
     },
   );
+  // https://stackoverflow.com/questions/40734263/how-to-set-primary-key-type-to-uuid-via-sequelize-cli
+  Team.beforeCreate(team => team.id = generateID())
   return Team;
 };
