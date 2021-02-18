1eca71d86675fedd0851d89c84d3080f8f6eba63
Patrick Du <patrickduu@gmail.com>
2021-02-18 00:58:18 -0500

    Make user table id a UUID

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-const { Model } = require('sequelize');
+const { Model, Sequelize } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -13,6 +13,10 @@ module.exports = (sequelize, DataTypes) => {
   }
   User.init(
     {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING,
       email: DataTypes.STRING,
