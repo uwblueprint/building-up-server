8bad6c96c76fcb398506118fe2446eade607b900
Cynthia Ding <cy2ding@uwaterloo.ca>
2021-02-18 21:40:09 -0500

    Adding associations for foreign keys

--- src/models/team.js
+++ src/models/team.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Team.hasMany(models.User);
+      Team.hasMany(models.Order);
     }
   }
   Team.init(
--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.belongsTo(models.Team);
+      User.hasMany(models.Order);
     }
   }
   User.init(
