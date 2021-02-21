dfefeacaaf2a3d0ae65b4d37779247cab2da2a45
Hong Yi Chen <hongyigma@gmail.com>
2021-02-20 20:06:40 -0500

    Trying to deploy using URI instead

--- src/models/index.js
+++ src/models/index.js
@@ -7,25 +7,26 @@ const {
   POSTGRES_PROD_PASSWORD,
   POSTGRES_PROD_HOST,
   POSTGRES_PROD_PORT,
+  POSTGRES_PROD_URI,
 } = require('../config/config');
 
 const basename = path.basename(__filename);
 const db = {};
 
-const sequelize = new Sequelize({
-  database: POSTGRES_PROD_DB,
-  username: POSTGRES_PROD_USER,
-  password: POSTGRES_PROD_PASSWORD,
-  host: POSTGRES_PROD_HOST,
-  port: POSTGRES_PROD_PORT,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = new Sequelize(POSTGRES_PROD_URI);
+// database: POSTGRES_PROD_DB,
+// username: POSTGRES_PROD_USER,
+// password: POSTGRES_PROD_PASSWORD,
+// host: POSTGRES_PROD_HOST,
+// port: POSTGRES_PROD_PORT,
+// dialect: 'postgres',
+// dialectOptions: {
+//   ssl: {
+//     require: true,
+//     rejectUnauthorized: false,
+//   },
+// },
+// });
 
 fs.readdirSync(__dirname)
   .filter(file => {
