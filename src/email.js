11da4f6a8ad8e32a75e29f493bf34cb3abf86baa
Hong Yi Chen <hongyigma@gmail.com>
2021-03-07 17:41:06 -0500

    Implemented test email using sendgrid

---
+++ src/email.js
@@ -0,0 +1,26 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const testMsg = {
+  to: 'hongyigma@gmail.com', // Change to your recipient
+  from: 'hongyichen@uwblueprint.org', // This is currently the verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
+const sendEmail = msg => {
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+sendEmail(testMsg);
