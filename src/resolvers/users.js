ce54bb8d7d387d5bc879a569ec27f88640cb25d6
Cynthia Ding <cy2ding@uwaterloo.ca>
2021-03-28 16:53:50 -0400

    Adding sendVerificationEmail logic to register	321ef8c	Cherry pick

--- src/resolvers/users.js
+++ src/resolvers/users.js
@@ -1,26 +1,6 @@
 const { UserInputError } = require('apollo-server-errors');
 const models = require('../models');
-const { sendEmail } = require('../services/sendEmail');
-
-// TO DO: Edit inivitation email subject, html, and generate email verification link
-const createVerificationEmail = id => {
-  const user = models.User.findByPk(id);
-  if (user == null) {
-    throw new UserInputError('User not found');
-  }
-
-  const hash = user.verificationHash;
-
-  if (hash == null) {
-    // TODO: hash not found?
-  }
-
-  return {
-    from: 'hongyichen@uwblueprint.org',
-    subject: `Verify Email for Building Up`,
-    html: 'Thank you for signing up with <strong>Building Up</strong>. Please join using this link: ',
-  };
-};
+const { createVerificationEmail, sendEmail } = require('../services/sendEmail');
 
 const usersResolvers = {
   Query: {
--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,9 +1,31 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
+const { UserInputError } = require('apollo-server-errors');
 const sgMail = require('@sendgrid/mail');
 const { SENDGRID_API_KEY } = require('../config/config');
+const models = require('../models');
 
 sgMail.setApiKey(SENDGRID_API_KEY);
+
+// TO DO: Edit inivitation email subject, html, and generate email verification link
+const createVerificationEmail = id => {
+  const user = models.User.findByPk(id);
+  if (user == null) {
+    throw new UserInputError('User not found');
+  }
+
+  const hash = user.verificationHash;
+
+  if (hash == null) {
+    // TODO: hash not found?
+  }
+
+  return {
+    from: 'hongyichen@uwblueprint.org',
+    subject: `Verify Email for Building Up`,
+    html: 'Thank you for signing up with <strong>Building Up</strong>. Please join using this link: ',
+  };
+};
 
 const sendEmail = async msg => {
   return sgMail
@@ -17,4 +39,5 @@ const sendEmail = async msg => {
     });
 };
 
+exports.createVerificationEmail = createVerificationEmail;
 exports.sendEmail = sendEmail;
