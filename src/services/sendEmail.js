9d8e599eace270aa5074b4e553fc9e638f70237a
Jeffrey Huang <jeff.huang62@gmail.com>
2021-03-29 21:45:25 -0400

    QoL use env variable

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,8 +1,9 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 const sgMail = require('@sendgrid/mail');
+const { SENDGRID_API_KEY } = require('../config/config');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async msg => {
   return sgMail
